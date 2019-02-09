215610057cd2333eb62f5bb6469be25e5bdea0b7
bsaul <bradleysaul@gmail.com>
2019-02-09 11:18:57 -0500

    updates documentation

--- R/ocwaste.R
+++ R/ocwaste.R
@@ -3,3 +3,23 @@
 #'
 
 "_PACKAGE"
+
+
+#' Data on NC Waste Management 2016-17
+#'
+#' From the  North Carolina Department of Environmental Quality Division of Waste Management
+#' report on County Population, Waste Disposal, Per Capita Rate and Percent Reduction, FY 2016-2017
+#' @source https://edocs.deq.nc.gov/WasteManagement/0/edoc/996950/NC_SWMMAR_FY2016-17_CountyPerCapitaReport.pdf?searchid=b1697dcf-7917-4c91-950e-fb192ed909de
+
+"nc_msw_cd_1617_report"
+
+
+#' Data on NC Recycling 2016-17
+#'
+#' From the  North Carolina Department of Environmental Quality Division of Waste Management
+#' report on County Population, Waste Disposal, Per Capita Rate and Percent Reduction, FY 2016-2017
+#' @source https://edocs.deq.nc.gov/WasteManagement/0/edoc/996950/NC_SWMMAR_FY2016-17_CountyPerCapitaReport.pdf?searchid=b1697dcf-7917-4c91-950e-fb192ed909de
+
+"nc_recycling_1617_report"
+"nc_recycling_1617_report"
+"nc_recycling_1617_report"
